cdb1b24ffd6b845684d38dfdd9988d6060d6c294
Javiera Vasquez <javiera@alba-app.com>
2019-05-16 16:40:21 -0400

    chore(TodosContext) remove unused code

--- src/TodosContextProvider/TodosContext.tsx
+++ src/TodosContextProvider/TodosContext.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
 
 export interface Todo {
   id: string;
@@ -7,67 +8,17 @@ export interface Todo {
 }
 
 export interface TodosContextInterface {
-  todos?: Todo[];
-  activeTodo?: string;
+  todos: Query;
+  isLoading: boolean;
+  activeTodo: string | undefined;
+  setActiveTodo: (id: string) => void;
   createTodo: (title: string) => void;
   updateTodo: (id: string, completed: boolean) => void;
   deleteTodo: (id: string) => void;
+  mutationWrappers: { [key: string]: Mutation };
 }
 
 const context = React.createContext<TodosContextInterface>(null as any);
 
 export const TodosContextProvider = context.Provider;
 export const TodosContextConsumer = context.Consumer;
-
-// const createTodo = ({ render }) => (
-//   <Mutation
-//     mutation={CREATE_TODO}
-//     update={(cache, { data: { createTodo Todo } }) => {
-//       const query = ALL_TODOS
-//       const { todos } = cache.readQuery({ query })
-
-//       cache.writeQuery({
-//         query,
-//         data: { todos: R.concat(todos, [createTodo]) },
-//       })
-//     }}
-//   >
-//     {(mutation, result) => render({ mutation, result })}
-//   </Mutation>
-// )
-
-// const updateTodo = ({ render }) => (
-//   <Mutation
-//     mutation={UPDATE_TODO}
-//     update={(cache, { data: { updateTodo } }) => {
-//       const query = ALL_TODOS
-//       const { todos } = cache.readQuery({ query })
-//       const idx = R.findIndex(R.propEq('id', updateTodo.id), todos)
-
-//       cache.writeQuery({
-//         query,
-//         data: { todos: R.update(idx, updateTodo, todos) },
-//       })
-//     }}
-//   >
-//     {(mutation, result) => render({ mutation, result })}
-//   </Mutation>
-// )
-
-// const deleteTodo = ({ render }) => (
-//   <Mutation
-//     mutation={DELETE_TODO}
-//     update={(cache, { data: { deleteTodo } }) => {
-//       const query = ALL_TODOS
-//       const { todos } = cache.readQuery({ query })
-//       const byTodoId = R.propEq('id', deleteTodo.id)
-
-//       cache.writeQuery({
-//         query,
-//         data: { todos: R.reject(byTodoId, todos) },
-//       })
-//     }}
-//   >
-//     {(mutation, result) => render({ mutation, result })}
-//   </Mutation>
-// )
