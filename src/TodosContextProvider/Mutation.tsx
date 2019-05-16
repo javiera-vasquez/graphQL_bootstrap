f491a5c1c571759cf474f8e3a94a25c54877c8e3
Javiera Vasquez <javiera@alba-app.com>
2019-05-16 13:15:51 -0400

    fix(Query) add interpolation to query fragment

--- src/TodosContextProvider/Mutation.tsx
+++ src/TodosContextProvider/Mutation.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { gql } from 'apollo-boost'
 import { TODO_FRAGMENT } from '../Fragment/commonFragments';
 
 export const CREATE_TODO = gql`
   mutation createTodo($title: String!) {
     createTodo(title: $title) {
-      ...TODO_FRAGMENT
+      ...${TODO_FRAGMENT}
     }
   }
 `
@@ -13,7 +12,7 @@ export const CREATE_TODO = gql`
 export const UPDATE_TODO = gql`
   mutation updateTodo($id: ID!, $completed: Boolean) {
     updateTodo(id: $id, completed: $completed) {
-      ...TODO_FRAGMENT
+      ...${TODO_FRAGMENT}
     }
   }
 `
--- src/TodosContextProvider/Query.tsx
+++ src/TodosContextProvider/Query.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { gql} from 'apollo-boost'
 import { TODO_FRAGMENT } from '../Fragment/commonFragments';
 
 export const ALL_TODOS = gql`
   {
     todos: allTodoes {
-      ...TODO_FRAGMENT
+      ...${TODO_FRAGMENT}
     }
   }
 `
