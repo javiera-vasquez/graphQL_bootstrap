1c2b5c165acb017331df297a8adac8d511c0d479
Javiera Vasquez <javiera@alba-app.com>
2019-05-16 13:22:32 -0400

    feat(TodosContext) add adopt map resolver to context provider

--- src/TodosContextProvider/TodosConsumerProvider.tsx
+++ src/TodosContextProvider/TodosConsumerProvider.tsx
@@ -1,33 +1,71 @@
 import React from 'react';
 import * as R from 'ramda';
+import { DataProxy } from 'apollo-cache';
+import { FetchResult } from 'apollo-link';
+import { Query } from 'react-apollo';
 
 import { ALL_TODOS } from './Query';
 import { UPDATE_TODO, DELETE_TODO, CREATE_TODO } from './Mutation';
+import { MutationFactory, QueryFactory } from '../helpers/ApolloFactory'
 
 import { TodosContextProvider, Todo } from './TodosContext';
+import { adopt } from 'react-adopt'
 
+const updateTodosCache = (cache: DataProxy, { data: { createTodo } }: FetchResult ) => {
+  const query = ALL_TODOS;
+  const { todos } = cache.readQuery({ query }) as any;
 
-// export const TodosContainer = adopt({
-//   todos: <Query query={ALL_TODOS} />,
-//   createTodo,
-//   updateTodo,
-//   deleteTodo,
-// })
+  cache.writeQuery({
+    query,
+    data: { todos: R.concat(todos, [createTodo]) },
+  })
+}
 
-export const TodosContextResolver = ({...props}) => (
-  <TodosContextProvider value={{
-    todos: [{
-      id: '1',
-      title: 'asdf',
-      completed: false
-    }],
-    activeTodo: undefined,
-    createTodo: () => {},
-    updateTodo: () => {},
-    deleteTodo: () => {}
-  }}>
-    { props.children }
-  </TodosContextProvider>
-);
+const updateTodoCache = (cache: DataProxy, { data: { updateTodo } }: FetchResult) => {
+  const query = ALL_TODOS;
+  const { todos } = cache.readQuery({ query }) as any;
+  const idx = R.findIndex(R.propEq('id', updateTodo.id), todos);
+
+  cache.writeQuery({
+    query,
+    data: { todos: R.update(idx, updateTodo, todos) },
+  })
+}
+
+const deleteTodosCache = (cache: DataProxy, { data: { deleteTodo } }: FetchResult) => {
+  const query = ALL_TODOS
+  const { todos } = cache.readQuery({ query }) as any;
+  const byTodoId = R.propEq('id', deleteTodo.id)
+
+  cache.writeQuery({
+    query,
+    data: { todos: R.reject(byTodoId, todos) },
+  })
+}
+
+export const TodosContainer = adopt({
+  todos: QueryFactory(ALL_TODOS),
+  createTodo: MutationFactory(CREATE_TODO, updateTodosCache),
+  updateTodo: MutationFactory(UPDATE_TODO, updateTodosCache),
+  deleteTodo: MutationFactory(DELETE_TODO, deleteTodosCache)
+})
+
+export const TodosContextResolver = ({...props}) => {
+  return (
+    <TodosContextProvider value={{
+      todos: [{
+        id: '1',
+        title: 'asdf',
+        completed: false
+      }],
+      activeTodo: undefined,
+      createTodo: () => {},
+      updateTodo: () => {},
+      deleteTodo: () => {}
+    }}>
+      { props.children }
+    </TodosContextProvider>
+  )
+};
 
 export default TodosContextResolver;
