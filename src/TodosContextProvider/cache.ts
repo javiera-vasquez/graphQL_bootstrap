76672a849a0fd2c8204912ae85437c80b28e0936
Javiera Vasquez <javiera@alba-app.com>
2019-05-16 16:39:22 -0400

    feat(TodosContext) add cache handle fn

---
+++ src/TodosContextProvider/cache.ts
@@ -0,0 +1,39 @@
+
+import * as R from 'ramda';
+import { DataProxy } from 'apollo-cache';
+import { FetchResult } from 'apollo-link';
+
+import { ALL_TODOS } from './Query';
+import { Todo } from './TodosContext';
+
+export const createTodoCache = (cache: DataProxy, { data: { createTodo } }: FetchResult ) => {
+  const query = ALL_TODOS;
+  const { todos } = cache.readQuery({ query }) as any;
+
+  cache.writeQuery({
+    query,
+    data: { todos: R.concat(todos, [createTodo]) },
+  })
+}
+
+export const updateTodoCache = (cache: DataProxy, { data: { updateTodo } }: FetchResult) => {
+  const query = ALL_TODOS;
+  const { todos } = cache.readQuery({ query }) as any;
+  const idx = R.findIndex(R.propEq('id', updateTodo.id), todos);
+
+  cache.writeQuery({
+    query,
+    data: { todos: R.update(idx, updateTodo, todos) },
+  })
+}
+
+export const deleteTodosCache = (cache: DataProxy, { data: { deleteTodo } }: FetchResult) => {
+  const query = ALL_TODOS
+  const { todos } = cache.readQuery({ query }) as any;
+  const byTodoId = R.propEq('id', deleteTodo.id)
+
+  cache.writeQuery({
+    query,
+    data: { todos: R.reject(byTodoId, todos) },
+  })
+}
