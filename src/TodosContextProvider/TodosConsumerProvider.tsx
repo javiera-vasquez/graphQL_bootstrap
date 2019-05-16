fcbaaf3d5fe87e02491b6ffb49f008766072e296
Javiera Vasquez <javiera@alba-app.com>
2019-05-16 16:40:49 -0400

    feat(TodosProvider) add handler functions to todosApi

--- src/TodosContextProvider/TodosConsumerProvider.tsx
+++ src/TodosContextProvider/TodosConsumerProvider.tsx
@@ -1,70 +1,62 @@
-import React from 'react';
-import * as R from 'ramda';
-import { DataProxy } from 'apollo-cache';
-import { FetchResult } from 'apollo-link';
-import { Query } from 'react-apollo';
+import React, { useState } from 'react';
+import { adopt } from 'react-adopt'
 
 import { ALL_TODOS } from './Query';
 import { UPDATE_TODO, DELETE_TODO, CREATE_TODO } from './Mutation';
 import { MutationFactory, QueryFactory } from '../helpers/ApolloFactory'
-
-import { TodosContextProvider, Todo } from './TodosContext';
-import { adopt } from 'react-adopt'
-
-const updateTodosCache = (cache: DataProxy, { data: { createTodo } }: FetchResult ) => {
-  const query = ALL_TODOS;
-  const { todos } = cache.readQuery({ query }) as any;
-
-  cache.writeQuery({
-    query,
-    data: { todos: R.concat(todos, [createTodo]) },
-  })
-}
-
-const updateTodoCache = (cache: DataProxy, { data: { updateTodo } }: FetchResult) => {
-  const query = ALL_TODOS;
-  const { todos } = cache.readQuery({ query }) as any;
-  const idx = R.findIndex(R.propEq('id', updateTodo.id), todos);
-
-  cache.writeQuery({
-    query,
-    data: { todos: R.update(idx, updateTodo, todos) },
-  })
-}
-
-const deleteTodosCache = (cache: DataProxy, { data: { deleteTodo } }: FetchResult) => {
-  const query = ALL_TODOS
-  const { todos } = cache.readQuery({ query }) as any;
-  const byTodoId = R.propEq('id', deleteTodo.id)
-
-  cache.writeQuery({
-    query,
-    data: { todos: R.reject(byTodoId, todos) },
-  })
-}
+import { createTodoCache, updateTodoCache, deleteTodosCache } from './cache'
+import { TodosContextProvider } from './TodosContext';
 
 export const TodosContainer = adopt({
-  todos: QueryFactory(ALL_TODOS),
-  createTodo: MutationFactory(CREATE_TODO, updateTodosCache),
-  updateTodo: MutationFactory(UPDATE_TODO, updateTodosCache),
+  todos: QueryFactory({ query: ALL_TODOS }),
+  createTodo: MutationFactory(CREATE_TODO, createTodoCache),
+  updateTodo: MutationFactory(UPDATE_TODO, updateTodoCache),
   deleteTodo: MutationFactory(DELETE_TODO, deleteTodosCache)
 })
 
 export const TodosContextResolver = ({...props}) => {
+  const [activeTodo, setActiveTodo] = useState<string | undefined>(undefined);
+  const [mutationStatus, setMutationStatus] = useState<boolean>(false);
+
   return (
-    <TodosContextProvider value={{
-      todos: [{
-        id: '1',
-        title: 'asdf',
-        completed: false
-      }],
-      activeTodo: undefined,
-      createTodo: () => {},
-      updateTodo: () => {},
-      deleteTodo: () => {}
-    }}>
-      { props.children }
-    </TodosContextProvider>
+    <TodosContainer>
+      {({ todos, createTodo, updateTodo, deleteTodo }: any) => {
+
+        const handleComplete = async (id: string, completed: boolean) => {
+          setMutationStatus(true)
+          await updateTodo.mutation({ variables: { id, completed: !completed } })
+          setMutationStatus(false)
+        }
+
+        const handleRemove = async (id: string) => {
+          setMutationStatus(true)
+          await deleteTodo.mutation({ variables: { id } })
+          setMutationStatus(false)
+        }
+
+        const handleCreate = async (title: string) => {
+          setMutationStatus(true)
+          await createTodo.mutation({ variables: { title } })
+          setMutationStatus(false)
+        }
+
+        return (
+          <TodosContextProvider value={{
+            todos,
+            activeTodo,
+            isLoading: mutationStatus,
+            setActiveTodo: (id: string) => setActiveTodo(id),
+            createTodo: handleCreate,
+            updateTodo: handleComplete,
+            deleteTodo: handleRemove,
+            mutationWrappers: { createTodo, updateTodo, deleteTodo }
+          }}>
+            { props.children }
+          </TodosContextProvider>
+
+        )
+      }}
+    </TodosContainer>
   )
 };
 
